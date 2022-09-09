e20960b3624ea081a394b36700187ef050d4acb1
Christopher C Ray <110488249+cray412@users.noreply.github.com>
2022-09-08 20:50:30 -0400

    created questions array

--- index.js
+++ index.js
@@ -6,8 +6,37 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
+{
+    type: 'input',
+    message: 'PLease enter the title of your project:',
+    name: 'title'
+},
+{
+    type: 'input',
+    message: 'PLease enter a description of your project:',
+    name: 'description'
+},
+{
+    type: 'input',
+    message: 'PLease enter installation instructions for your project:',
+    name: 'installation'
+},
+{
+    type: 'input',
+    message: 'PLease enter the usage information for your project:',
+    name: 'usage'
+},
+{
+    type: 'input',
+    message: 'PLease enter the contribution guidelines for your project:',
+    name: 'guidelines'
+},
+{
+    type: 'input',
+    message: 'PLease enter the test instructions for your project:',
+    name: 'test'
+},
 
-    
 ];
 
 
